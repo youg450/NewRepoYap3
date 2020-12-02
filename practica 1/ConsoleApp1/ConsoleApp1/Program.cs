0abe7d9a4f6913f80076ecff18d14d4b564c6cae
tema <tema>
2020-12-02 17:33:33 +0300

    02.12

---
+++ practica 1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp1_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Person p = new Person("Tony", "Allen", 32, Person.Genders.Male);
+      Console.WriteLine(p.ToString());
+    }
+  }
+  struct Person
+  {
+
+    public string firstName;
+    public string lastName;
+    public int age;
+    public enum Genders : int { Male, Female };
+    public Genders gender;
+    public Person(string _firstName, string _lastName, int _age, Genders _gender)
+    {
+      firstName = _firstName; lastName =
+      _lastName; age = _age; gender = _gender;
+    }
+    public override string ToString()
+    {
+      return firstName + " " + lastName + " (" + gender + "), age " + age;
+    }
+  }
+}
---
+++ practica 1/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      SByte a = 0; 
+      Byte b = 0; 
+      Int16 c =0; 
+      Int32 d = 0; 
+      Int64 e = 0; 
+      string s= "";
+      Exception ex = new Exception();
+      object[] types = { a, b, c, d, e, s, ex };
+      foreach (object o in types)
+      {
+        string type;
+        if (o.GetType().IsValueType) type = "Value type";
+        else
+          type = "Reference Type";
+        Console.WriteLine("{0}: {1}", o.GetType(), type);
+      }
+    }
+  }
+}
